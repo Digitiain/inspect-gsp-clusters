50627e36ecf758d40a426f8be282f47f413a1f14
Flash <gordon.iainl@gmail.com>
2019-06-10 20:11:49 +0100

    Add Flask framework & get data working
    Add Flask framework
    Get GitHub API call working
    Add basic structure of cluster list and detail pages

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 #!/usr/bin/env python3
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
 import os
 import base64
 import yaml
 from github import Github
 
-# Github setup
-token = os.environ['GITHUB_TOKEN']
-g = Github(token)
 
-repo = g.get_repo("alphagov/gsp-teams")
-contents = repo.get_contents("clusters")
+@app.route('/')
+def home():
 
-for file in contents:
+	# Github setup
+	token = os.environ['GITHUB_TOKEN']
+	g = Github(token)
+
+	repo = g.get_repo("alphagov/gsp-teams")
+	contents = repo.get_contents("clusters")
+
+	clusters = []
+
+	for file in contents:
+		decoded = base64.b64decode(file.content)
+		yamled = yaml.safe_load(decoded)
+		print(yamled["cluster-name"])
+		clusters.append(yamled)
+
+	return render_template('home.html', clusters=clusters)
+
+
+@app.route('/<clustername>')
+def show_cluster_info(clustername):
+
+	# Github setup
+	token = os.environ['GITHUB_TOKEN']
+	g = Github(token)
+
+	repo = g.get_repo("alphagov/gsp-teams")
+	file = repo.get_contents("clusters/" + clustername + ".yaml")
 	decoded = base64.b64decode(file.content)
-	yamled = yaml.safe_load(decoded)
-	print(yamled["cluster-name"])
+	cluster = yaml.safe_load(decoded)
+
+	return render_template('detail.html', cluster=cluster)
